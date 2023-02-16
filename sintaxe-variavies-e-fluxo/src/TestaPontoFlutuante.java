c05010513810deca09fe252a9d6b9f29d044ab3c
duduzx02 <duduzx02@gmail.com>
2023-02-16 02:41:56 -0300

    Trabalhando com pontos flutuantes

---
+++ sintaxe-variavies-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,24 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.20;
+		
+		System.out.println("Meu salário é: " + salario);
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outroDivisao = 5/2;
+		System.out.println(outroDivisao);
+		
+		double novaTentativa = 5 / 2;
+		System.out.println(novaTentativa);
+		
+		double novaTentativaDenovo = 5.0 / 2;
+		System.out.println(novaTentativaDenovo);
+	}
+	
+
+}
