7ba85ec2dec52dab8e84b6c72abb7b6a3e91ab12
duduzx02 <duduzx02@gmail.com>
2023-02-16 01:49:41 -0300

    Sintaxe de variáveis

---
+++ sintaxe-variavies-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("OI");
+		
+		int idade;
+		idade = 26;
+		
+		System.out.println("A idade é " + idade + ", parabolas");
+	}
+
+}
