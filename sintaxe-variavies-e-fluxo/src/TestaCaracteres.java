123396a485092f13a5ed1c441509a57e475fb332
duduzx02 <duduzx02@gmail.com>
2023-02-16 03:24:56 -0300

    Trabalhando com char e String

---
+++ sintaxe-variavies-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,22 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor  + 1);
+		System.out.println(valor);
+		
+		String frase = "Alura cursos on";
+		System.out.println(frase);
+		
+		frase = frase + 2020;
+		System.out.println(frase);
+		
+	}
+}
