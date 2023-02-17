37f65158835e54784c264b6ef4c6c0ad2722adee
duduzx02 <duduzx02@gmail.com>
2023-02-17 20:59:13 -0300

    Somatória de um Whilw

---
+++ sintaxe-variavies-e-fluxo/src/TestaSomatoria.java
@@ -0,0 +1,14 @@
+
+public class TestaSomatoria {
+
+	public static void main(String[] args) {
+		int somatoria = 0;
+		int contador = 0;
+		while(contador <= 10) {
+			somatoria += contador;
+			contador++;
+	
+		}
+		System.out.println(somatoria);
+	}
+}
