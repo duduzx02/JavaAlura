2ab9a17fbd4e0e7961173aa67c31f119fa79d0b6
duduzx02 <duduzx02@gmail.com>
2023-02-18 03:30:18 -0300

    Primeiro método

---
+++ BigBank/src/TestaMetodo.java
@@ -0,0 +1,12 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoEduardo = new Conta();
+		contaDoEduardo.deposita(50);
+		
+		System.out.println(contaDoEduardo.saldo);
+
+	}
+
+}
