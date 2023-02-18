06afb984d5a5ba6ec314a8a3f30e134626681444
duduzx02 <duduzx02@gmail.com>
2023-02-18 00:13:11 -0300

    Instanciação, atributos e referências

---
+++ BigBank/src/CriarConta.java
@@ -0,0 +1,10 @@
+
+public class CriarConta {
+
+	public static void main(String[] args) {
+		Conta PrimeiraConta = new Conta();
+		PrimeiraConta.saldo = 200;
+	
+		System.out.println(PrimeiraConta.saldo);
+	}
+}
