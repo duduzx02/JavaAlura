6014969b3ecb9d9b85d37a8ceac44117fcf25e23
duduzx02 <duduzx02@gmail.com>
2023-02-17 21:46:26 -0300

    Testando laços alinhados com break

--- sintaxe-variavies-e-fluxo/src/TestaConversao.java
+++ sintaxe-variavies-e-fluxo/src/TestaConversao.java
@@ -1,17 +1,17 @@
 
 public class TestaConversao {
-		public static void main(String[] args) {
-			double salario = 1270.20;
-			int valor = (int) salario; // casting
-			System.out.println(valor);
-			
-			long numeroLongo = 3545646746546l;
-			short valorPequeno = 2141;
-			byte bt = 127;
-			
-			double valor1 = 0.2;
-			double valor2 = 0.1;
-			
-			System.out.println(valor1 + valor2);
-		}
+	public static void main(String[] args) {
+		double salario = 1270.20;
+		int valor = (int) salario; // casting
+		System.out.println(valor);
+
+		long numeroLongo = 3545646746546l;
+		short valorPequeno = 2141;
+		byte bt = 127;
+
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+
+		System.out.println(valor1 + valor2);
 	}
+}
--- sintaxe-variavies-e-fluxo/src/TestandoLacos.java
+++ sintaxe-variavies-e-fluxo/src/TestandoLacos.java
@@ -2,10 +2,10 @@
 public class TestandoLacos {
 
 	public static void main(String[] args) {
-		for(int contador = 1; contador <= 10; contador++) {
-			for(int i = 1; i <= 10; i++) {
+		for (int contador = 1; contador <= 10; contador++) {
+			for (int i = 1; i <= 10; i++) {
 				int resultado = i * contador;
-				System.out.println(contador  + "x" + i + "=" + resultado);
+				System.out.println(contador + "x" + i + "=" + resultado);
 			}
 		}
 	}
