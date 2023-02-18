308b12ae2782714fb2746b370600e9e60abb931e
duduzx02 <duduzx02@gmail.com>
2023-02-17 22:00:40 -0300

    Fatorial

---
+++ sintaxe-variavies-e-fluxo/src/Fatorial.java
@@ -0,0 +1,12 @@
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		int fatorial = 1;
+		for(int contador = 1; contador <= 6; contador++) {
+			fatorial = fatorial * contador;
+			System.out.println(fatorial);
+		}
+	}
+
+}
