f1b2a4d413aa2a99b8862d4fd6c5dfe3f78c6271
duduzx02 <duduzx02@gmail.com>
2023-02-19 11:03:27 -0300

    Métodos com referência e mais retorno

--- BigBank/src/Conta.java
+++ BigBank/src/Conta.java
@@ -7,4 +7,25 @@ public class Conta{
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
+	
+	public boolean saca(double valor) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			System.out.println("Saldo insuficiente!");
+			return false;
+		}
+
+	}
+	
+	public boolean tranfere(double valor, Conta destino) {
+		if(this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- BigBank/src/TestaMetodo.java
+++ BigBank/src/TestaMetodo.java
@@ -6,7 +6,23 @@ public class TestaMetodo {
 		contaDoEduardo.deposita(50);
 		
 		System.out.println(contaDoEduardo.saldo);
-
+		
+		contaDoEduardo.saca(500);
+		System.out.println(contaDoEduardo.saldo);
+		
+		Conta contaDaLivia = new Conta();
+		contaDaLivia.deposita(500);
+		
+		
+		if(contaDaLivia.tranfere(300, contaDoEduardo)) {
+			System.out.println("Transferencia feita com sucesso!");
+		} else {
+			System.out.println("Saldo insuficiente!");
+		}
+		;
+		System.out.println(contaDaLivia.saldo);
+		System.out.println(contaDoEduardo.saldo);
+		
 	}
 
 }
