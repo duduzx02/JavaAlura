a828a3f24863601e4fe9e6b493e2becee904b048
duduzx02 <duduzx02@gmail.com>
2023-02-24 02:57:12 -0300

    Herdando classes com interfaces

--- BigBank-herdado/src/Autenticavel.java
+++ BigBank-herdado/src/Autenticavel.java
@@ -1,18 +1,13 @@
+// Contrato autenticavel
+	// Quem assina esse contrato, precisa implementar
+		// metodo setSenha
+		// metodo autentica
 
-public abstract class Autenticavel{
-	private int senha;
+public abstract interface Autenticavel{
 	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
+	public abstract void setSenha(int senha);
+	
+	public abstract boolean autentica(int senha);
 
-	}
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
 
 }
