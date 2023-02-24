890434e178d3f1e8913467870db3fee89e2b1c16
duduzx02 <duduzx02@gmail.com>
2023-02-23 22:41:00 -0300

    Classes abstratas

--- BigBank-herdado/src/TestaReferencias.java
+++ BigBank-herdado/src/TestaReferencias.java
@@ -7,10 +7,6 @@ public class TestaReferencias {
 		g1.setNome("Eduardo");
 		g1.setSalario(5000.0);
 		
-		Funcionario f1 = new Funcionario();
-		f1.setNome("Livia");
-		f1.setSalario(2000.0);
-		
 		Funcionario e1 = new Editor();
 		e1.setNome("Seila");
 		e1.setSalario(2500.0);
@@ -21,7 +17,6 @@ public class TestaReferencias {
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
-		controle.registra(f1);
 		controle.registra(e1);
 		controle.registra(p1);
 		
